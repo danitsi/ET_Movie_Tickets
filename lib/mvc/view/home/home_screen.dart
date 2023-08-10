789793886713f6263561936beb7746aab56bfcb8
Daniel Basazinew <danielbasazinew5@gmail.com>
2023-08-10 16:09:58 +0300

    unneccasry  files removed

--- lib/mvc/view/home/home_screen.dart
+++ lib/mvc/view/home/home_screen.dart
@@ -1,9 +1,5 @@
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:et_movie_tickets/mvc/controller/home/home_screen_controller.dart';
-import 'package:et_movie_tickets/mvc/view/home/movies_page.dart';
-import 'package:et_movie_tickets/mvc/view/home/profile_page.dart';
-import 'package:et_movie_tickets/mvc/view/home/theaters_page.dart';
-import 'package:et_movie_tickets/mvc/view/home/trailers_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -15,8 +11,8 @@ class HomeScreenPage extends StatefulWidget {
 
   @override
   State<HomeScreenPage> createState() => _HomeScreenPageState(selectedPage);
-
 }
+
 class _HomeScreenPageState extends State<HomeScreenPage> {
   int selectedPage;
 
@@ -35,66 +31,56 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
       ),
     );
 
-    return
-     Container(
-       child: DefaultTabController(
-         length: 4,
-         child: GetBuilder<HomeScreenController>(
-           builder: (controller) => Scaffold(
-           backgroundColor: Colors.black,
-            body: TabBarView
-              (
-           physics: NeverScrollableScrollPhysics(),
-              children: [
-               MoviesPage(),
-               TheatersPage(),
-                TrailersPage(),
-                ProfilePage(),
-              ],
-            ),
-             bottomNavigationBar: Container(
-             height: 60,
-               margin: EdgeInsets.only(bottom: Get.mediaQuery.viewPadding.bottom),
-               child: Container(
-                color: Colors.white,
-                child: TabBar(
-                  indicatorWeight: 0.1,
-                  automaticIndicatorColorAdjustment: false,
-                  unselectedLabelColor: Colors.black,
-                  labelStyle:const TextStyle(fontSize: 15),
-                  labelColor: Colors.white,
-                  onTap: (index) {
-                    controller.changeIcons(index);
-                  },
-                  tabs: <Widget>[
-                    Tab(
-                      icon: controller.moviesIcon,
-                      text: 'MOVIES'.tr,
-                      iconMargin: EdgeInsets.only(bottom: 0),
+    return Container(
+      child: DefaultTabController(
+        length: 4,
+        child: GetBuilder<HomeScreenController>(
+            builder: (controller) => Scaffold(
+                  backgroundColor: Colors.black,
+                  body: TabBarView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [],
+                  ),
+                  bottomNavigationBar: Container(
+                    height: 60,
+                    margin: EdgeInsets.only(
+                        bottom: Get.mediaQuery.viewPadding.bottom),
+                    child: Container(
+                      color: Colors.white,
+                      child: TabBar(
+                        indicatorWeight: 0.1,
+                        automaticIndicatorColorAdjustment: false,
+                        unselectedLabelColor: Colors.black,
+                        labelStyle: const TextStyle(fontSize: 15),
+                        labelColor: Colors.white,
+                        onTap: (index) {
+                          controller.changeIcons(index);
+                        },
+                        tabs: <Widget>[
+                          Tab(
+                            icon: controller.moviesIcon,
+                            text: 'MOVIES'.tr,
+                            iconMargin: EdgeInsets.only(bottom: 0),
+                          ),
+                          Tab(
+                              icon: controller.theatersIcon,
+                              text: 'THEATERS'.tr,
+                              iconMargin: EdgeInsets.only(bottom: 0)),
+                          Tab(
+                              icon: controller.trailersIcon,
+                              text: 'TRAILERS'.tr,
+                              iconMargin: EdgeInsets.only(bottom: 0)),
+                          Tab(
+                              icon: controller.profileIcon,
+                              text: 'PROFILE'.tr,
+                              iconMargin: EdgeInsets.only(bottom: 0)),
+                        ],
+                      ),
                     ),
-                    Tab(
-                        icon: controller.theatersIcon,
-                        text: 'THEATERS'.tr,
-                        iconMargin: EdgeInsets.only(bottom: 0)
-                    ),
-                    Tab(
-                        icon: controller.trailersIcon,
-                        text: 'TRAILERS'.tr,
-                        iconMargin: EdgeInsets.only(bottom: 0)
-                    ),
-                    Tab(
-                        icon: controller.profileIcon,
-                        text: 'PROFILE'.tr,
-                        iconMargin: EdgeInsets.only(bottom: 0)
-                    ),
-                  ],
-                ),
-               ),
-             ),
-           )
-         ),
-       ),
-     );
+                  ),
+                )),
+      ),
+    );
   }
 
   @override
@@ -110,18 +96,15 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
   }
 
   bool myInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
-    if(EasyLoading.isShow) {
+    if (EasyLoading.isShow) {
       print("BackButton clicked :- Loading is shown");
       return true;
     }
 
     print("BackButton clicked :- Get.currentRoute :-${Get.currentRoute}");
 
-
-    if(Get.currentRoute == "/" || Get.currentRoute == "/HomeTabbedPage") {
-
-    }
-    else {
+    if (Get.currentRoute == "/" || Get.currentRoute == "/HomeTabbedPage") {
+    } else {
       print("BackButton clicked :- Current Page :-${Get.currentRoute}");
       Get.back();
     }
